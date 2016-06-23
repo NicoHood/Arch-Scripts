3e0445d4feb8f63aed75495c987cc376c5410cb0
NicoHood <NicoHood@users.noreply.github.com>
2016-06-23 21:39:58 +0200

    Added backup

---
+++ backup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# TODO
+# Create snapshot
+# Mount snapshot
+# Backup snapshot
+# Delete snapshot
+# Do this again for the boot partition
+# Backup MBR (aka first 1MiB)
+# Backup lvm layout
+# Export luks headers
